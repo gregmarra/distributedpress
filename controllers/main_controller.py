9549c10573f3b5b88a8253b6aef6783605988400
Greg Marra <greg.marra@gmail.com>
2013-05-18 12:06:12 -0700

    Towards some user management stuff.

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -2,13 +2,21 @@ import os
 import logging
 import webapp2
 
+from google.appengine.api import users
 from google.appengine.ext import ndb
 from google.appengine.ext.webapp import template
 
 class MainController(webapp2.RequestHandler):
     def get(self):
+
+        users_dict = {
+            "login_url": users.create_login_url("/printers/list"),
+            "logout_url": users.create_logout_url("/"),
+        }
+
         template_values = {
-            "asdf": "cheese"
+            "user": users.get_current_user(),
+            "users_dict": users_dict,
         }
     
         path = os.path.join(os.path.dirname(__file__), '../templates/main.html')
