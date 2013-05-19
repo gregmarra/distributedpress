1cd4bcfbccdbb59d54ce0a5e157c80b4b3ce7229
Greg Marra <greg.marra@gmail.com>
2013-05-19 14:00:29 -0700

    Adds about page scaffold.

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -18,3 +18,14 @@ class MainController(webapp2.RequestHandler):
     
         path = os.path.join(os.path.dirname(__file__), '../templates/main.html')
         self.response.write(template.render(path, template_values))
+
+class AboutController(webapp2.RequestHandler):
+    def get(self):
+        user_bundle = UserBundle()
+
+        template_values = {
+            "user_bundle": user_bundle,
+        }
+    
+        path = os.path.join(os.path.dirname(__file__), '../templates/about.html')
+        self.response.write(template.render(path, template_values))
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import ann_config
 
 from controllers.admin_deliver_controller import AdminDeliverController
 from controllers.admin_printer_list_controller import AdminPrinterListController
-from controllers.main_controller import MainController
+from controllers.main_controller import AboutController, MainController
 from controllers.dashboard_controller import DashboardController
 from controllers.printer_add_controller import PrinterAddController
 from controllers.printer_delete_controller import PrinterDeleteController
@@ -12,6 +12,7 @@ from controllers.printer_test_print_controller import PrinterTestPrintController
 
 app = webapp2.WSGIApplication([
     ('/', MainController),
+    ('/about', AboutController),
     ('/admin/printers/list', AdminPrinterListController),
     ('/admin/deliver', AdminDeliverController),
     ('/dashboard', DashboardController),
